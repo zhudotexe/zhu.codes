b583ce315237496ba09ab262c50cd4a5080d6db8
Andrew Zhu <me@andrew-zhu.com>
2021-10-20 20:22:31 -0400

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <img alt="Vue logo" src="assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <bulma></bulma>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import bulma from './components/bulma.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    bulma
+  }
+});
+</script>
+
+<style lang="scss">
+@import "./global.scss";
+</style>
