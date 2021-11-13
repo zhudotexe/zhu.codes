8f4fdd608e136298dddbf9fbb69d70a095475203
Andrew Zhu <me@andrew-zhu.com>
2021-11-13 15:02:59 -0800

    chore(deps): add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,6 @@
 <template>
-  <Heading></Heading>
-
-  <SiteSection name="About Me">
-    foobar
-  </SiteSection>
-
-  <SiteSection name="Another Section">
-    blep
-  </SiteSection>
-
-  <BulmaDemo></BulmaDemo>
+  <router-view/>
 </template>
-
-<script lang="ts">
-import {defineComponent} from 'vue';
-import BulmaDemo from '@/components/BulmaDemo.vue'
-import Heading from "@/components/Heading.vue";
-import SiteSection from "@/components/SiteSection.vue";
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    SiteSection,
-    Heading,
-    BulmaDemo
-  }
-});
-</script>
 
 <style lang="scss">
 @import "./global.scss";
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <Heading></Heading>
+  <SiteSection name="About Me">
+    foobar
+  </SiteSection>
+  <SiteSection name="Another Section">
+    blep
+  </SiteSection>
+  <BulmaDemo></BulmaDemo>
+</template>
+
+<script>
+import BulmaDemo from '@/components/BulmaDemo.vue'
+import Heading from "@/components/Heading.vue";
+import SiteSection from "@/components/SiteSection.vue";
+
+export default {
+  name: 'Home',
+  components: {
+    SiteSection,
+    Heading,
+    BulmaDemo
+  }
+}
+</script>
