55c5a2afd72069acf9ed46fbe25f495ec7d3964b
Andrew Zhu <me@andrew-zhu.com>
2022-05-29 17:45:32 -0700

    chore: begin separating paissa and worldtable

--- src/views/paissa/PaissaViewer.vue
+++ src/views/paissa/PaissaViewer.vue
@@ -68,44 +68,7 @@
           <!-- /# info -->
 
           <!-- table -->
-          <div class="table-container mt-4">
-            <table class="table is-striped is-fullwidth is-hoverable">
-              <thead>
-              <tr>
-                <th>Address</th>
-                <th>Size</th>
-                <th>Price</th>
-                <th>Entries</th>
-                <th>Lottery Phase</th>
-                <th>Allowed Tenants</th>
-                <th>Last Updated</th>
-              </tr>
-              </thead>
-
-              <tbody>
-              <tr v-for="plot in selectedWorldPlots"
-                  :key="[plot.world_id, plot.district_id, plot.ward_number, plot.plot_number]">
-                <td>
-                  {{ client.districtName(plot.district_id) }}, Ward {{ plot.ward_number + 1 }}, Plot
-                  {{ plot.plot_number + 1 }}
-                </td>
-                <td>{{ utils.sizeStr(plot.size) }}</td>
-                <td>{{ plot.price.toLocaleString() }}</td>
-                <td>
-                  <FlashOnChange :value="utils.lotteryEntryCountStr(plot)"
-                                 :class="{'is-italic': utils.shouldEm(plot)}"/>
-                </td>
-                <td>
-                  <FlashOnChange :value="utils.lotteryPhaseStr(plot)" :class="{'is-italic': utils.shouldEm(plot)}"/>
-                </td>
-                <td>{{ utils.tenantStr(plot.purchase_system) }}</td>
-                <td>
-                  <FlashOnChange :value="utils.updatedStr(plot.last_updated_time)"/>
-                </td>
-              </tr>
-              </tbody>
-            </table>
-          </div>
+          <WorldTable :client="client" :plots="selectedWorldPlots"/>
           <!-- /table -->
         </template>
       </template>
@@ -121,12 +84,13 @@ import FlashOnChange from "@/components/FlashOnChange.vue";
 import {PaissaClient, PlotState} from "@/views/paissa/client";
 import {WorldSummary} from "@/views/paissa/types";
 import * as utils from "@/views/paissa/utils";
+import WorldTable from "@/views/paissa/WorldTable.vue";
 import {groupBy} from "lodash";
 import {defineComponent} from "vue";
 
 export default defineComponent({
   name: "PaissaViewer",
-  components: {Dropdown, FlashOnChange},
+  components: {WorldTable, Dropdown, FlashOnChange},
   data() {
     return {
       client: new PaissaClient(),
