ed15a2fdaf4ed793a04563a736703a207095b125
Andrew Zhu <me@andrew-zhu.com>
2022-05-31 19:38:34 -0700

    feat: add titles to pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,37 @@
 import Home from '@/views/Home.vue'
 import NotFound from '@/views/NotFound.vue'
 import PaissaViewer from '@/views/paissa/PaissaViewer.vue'
+import {nextTick} from "vue";
 import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+
+const DEFAULT_TITLE = "zhu.codes";
 
 const routes: Array<RouteRecordRaw> = [
     {path: '/', name: 'Home', component: Home},
-    {path: '/paissa', name: 'PaissaViewer', component: PaissaViewer},
-    // {
-    //   path: '/about',
-    //   name: 'About',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    // }
-    {path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound}
+    {
+        path: '/paissa',
+        name: 'PaissaViewer',
+        component: () => import(/* webpackChunkName: "paissa" */ '../views/paissa/PaissaViewer.vue'),
+        meta: {title: 'PaissaDB - zhu.codes'}
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFound,
+        meta: {title: '404 - zhu.codes'}
+    }
 ]
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
 })
+
+
+router.afterEach((to) => {
+    nextTick(() => {
+        document.title = (to.meta.title as string) || DEFAULT_TITLE;
+    });
+});
 
 export default router
